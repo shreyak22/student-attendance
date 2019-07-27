8ee195332e2b8ecf29646e6442098aaf81ef1138
shreyak22 <contact.shreya22@gmail.com>
2019-07-27 17:37:55 -0500

    added spring boot starter tools

---
+++ src/main/java/com/shreyauntwale/studentattendance/StudentAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.shreyauntwale.studentattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyauntwale/studentattendance/StudentAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyauntwale.studentattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
